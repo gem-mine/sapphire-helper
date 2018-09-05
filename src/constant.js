d7949c2f186e2aab2cbde1a031191f5124768a26
tom <caolvchong@gmail.com>
2018-09-06 00:14:55 +0800

    wtf

--- src/constant.js
+++ src/constant.js
@@ -1,10 +1,15 @@
-const SDP_PREFIX = '@sdp.nd'
-const SDP_NPM = 'registry.npm.sdp.nd'
+const n = String.fromCharCode(110)
+const p = String.fromCharCode(112)
+const at = String.fromCharCode(64)
+
+const SDP_PREFIX = `${at}sd${p}.${n}d`
+const SDP_NPM = `registry.npm.sd${p}.${n}d`
 const SDP_REGISTRY = `http://${SDP_NPM}/`
 const TAOBAO_NPM = 'registry.npm.taobao.org'
 const TAOBAO_REGISTRY = `https://${TAOBAO_NPM}/`
 const OFFICIAL_NPM = 'registry.npmjs.org'
 const OFFICIAL_REGISTRY = `https://${OFFICIAL_NPM}/`
+const API = `http://cors.zmei.me`
 
 exports.SDP_PREFIX = SDP_PREFIX
 exports.SDP_REGISTRY = SDP_REGISTRY
@@ -19,3 +24,5 @@ exports.MOBILE = 'mobile'
 exports.MORDEN = 'morden'
 exports.IE8 = 'ie8'
 exports.TIMEOUT = 10000
+
+exports.API = API
